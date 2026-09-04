f84b87a89eb2b3ac9e983c78d61f4a048dd13dcd
princecabaya <princejobetrohcabayacruz@gmail.com>
2026-09-04 23:31:14 +0800

    Configure production Apps Script endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.USG_ATTENDANCE_CONFIG = Object.freeze({
-  https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRMJ1RSf4cn5bjo3SSTouuKWBsn2-BIu45oO7P6QFbaVgPJkH57m-jrHww6ao5_j0GsxeGpi1mRjFju3vI2rS_WOUIg9ZnSavPX_gdQ8eDGkmU1awsf5WlIQln7wT3HlW8VZGjslnCFj_1rIGKuO12aWcfk_fUDsHxyX5DG54bwurXdy9xo02I25Zkf6rr9ERLmWE8xZBr1EWc-Yv86RcdlIB3tE2HwZmV5BFKSZz2dMiLEcjel-8AdMV2GUHtqeEyqRHHYm0V1RmjN3RrnCx1AtsK9ww&lib=MpH_kqPlOug57_smfRQ0LOllznRPClwY4
+  // Permanent Google Apps Script web app deployment URL.
   // The Control Dashboard also accepts a device-only override during setup.
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzUWHW8RVzW58gQQIWSn6krWFczh8zeqndmDpYc_6nQsKFUmbz1UQf874tWNNw9Uz4V/exec",
   campusName: "Palawan State University – Balabac Campus",
   organizationName: "University Student Government",
 });
