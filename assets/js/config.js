e2afba5c41d4f1909b86a056f625be82c03f2e1f
princecabaya <princejobetrohcabayacruz@gmail.com>
2026-09-04 23:13:20 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.USG_ATTENDANCE_CONFIG = Object.freeze({
-  // Paste the /exec URL from the deployed Google Apps Script web app here.
+  // https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRMJ1RSf4cn5bjo3SSTouuKWBsn2-BIu45oO7P6QFbaVgPJkH57m-jrHww6ao5_j0GsxeGpi1mRjFju3vI2rS_WOUIg9ZnSavPX_gdQ8eDGkmU1awsf5WlIQln7wT3HlW8VZGjslnCFj_1rIGKuO12aWcfk_fUDsHxyX5DG54bwurXdy9xo02I25Zkf6rr9ERLmWE8xZBr1EWc-Yv86RcdlIB3tE2HwZmV5BFKSZz2dMiLEcjel-8AdMV2GUHtqeEyqRHHYm0V1RmjN3RrnCx1AtsK9ww&lib=MpH_kqPlOug57_smfRQ0LOllznRPClwY4
   // The Control Dashboard also accepts a device-only override during setup.
   apiUrl: "",
   campusName: "Palawan State University – Balabac Campus",
